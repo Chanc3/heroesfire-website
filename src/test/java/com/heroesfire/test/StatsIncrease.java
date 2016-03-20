59fb6644857e7cc204521d83fffe7218fe9fe0d5
Chance Shiflett <chanceshiflett@yahoo.com>
2016-03-20 16:20:34 -0700

    create StatsIncrease class

---
+++ src/test/java/com/heroesfire/test/StatsIncrease.java
@@ -0,0 +1,20 @@
+package com.heroesfire.test;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class StatsIncrease {
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+}
