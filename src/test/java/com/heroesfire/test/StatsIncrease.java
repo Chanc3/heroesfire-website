66d698ba038840e08adf22a56cbb5b17421dd951
Chance Shiflett <chanceshiflett@yahoo.com>
2016-03-28 15:17:13 -0700

    Add groups for test suite

--- src/test/java/com/heroesfire/test/StatsIncrease.java
+++ src/test/java/com/heroesfire/test/StatsIncrease.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -29,27 +30,42 @@ public class StatsIncrease {
 
 	private String BASE_URL = "http://www.heroesfire.com";
 
-	private WebDriver driver = new FirefoxDriver();
+	private WebDriver driver;
 
-	private WebDriverWait wait = new WebDriverWait(driver, 10);
+	private WebDriverWait wait;
 
-	private Actions action = new Actions(driver);
+	private Actions action;
 
-	@AfterClass
+	@AfterClass(groups = { "firefox", "chrome" })
 	public void afterClass() {
 		driver.quit();
 	}
 
-	@BeforeClass
-	public void beforeClass() {
+	@BeforeClass(groups = { "chrome" })
+	public void beforeClassChrome() {
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/chance/Dropbox/sqaworkspace/heroesfire-website/Drivers/chromedriver");
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 10);
+		action = new Actions(driver);
+		driver.get(BASE_URL);
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".self-clear>.megamenu a[href*='talent-calculator']")))
+				.click();
 	}
 
-	@DataProvider
-	public Object[][] healthData() {
-		return new Object[][] { new Object[] { "abathur" }, new Object[] { "jaina" }, new Object[] { "butcher" } };
+	@BeforeClass(groups = { "firefox" })
+	public void beforeClassFirefox() {
+		driver = new FirefoxDriver();
+		wait = new WebDriverWait(driver, 10);
+		action = new Actions(driver);
+		driver.get(BASE_URL);
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".self-clear>.megamenu a[href*='talent-calculator']")))
+				.click();
 	}
 
-	@Test(dataProvider = "healthData")
+	@Test(dataProvider = "heroData", groups = { "firefox", "chrome" })
 	public void healthIncreaseTest(String hero) {
 		ArrayList<WebElement> stats = new ArrayList<WebElement>();
 		ArrayList<String> beforeStats = new ArrayList<String>();
@@ -58,10 +74,6 @@ public class StatsIncrease {
 		boolean statIncreased = true;
 		int level;
 		double total;
-		driver.get(BASE_URL);
-		wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.cssSelector(".self-clear>.megamenu a[href*='talent-calculator']")))
-				.click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".choose-hero a[href*=" + hero)))
 				.click();
 		stats.add(wait.until(ExpectedConditions
@@ -73,8 +85,55 @@ public class StatsIncrease {
 		action.dragAndDropBy(
 				wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ui-slider-handle"))), 20, 0)
 				.perform();
-		stats.addAll(wait.until(ExpectedConditions
-				.visibilityOfAllElementsLocatedBy(By.cssSelector(".stats.mb10>div:nth-child(1) [data-gain]"))));
+		stats.add(wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".stats.mb10>div:nth-child(1) [data-gain]"))));
+		for (WebElement stat : stats) {
+			if (!stat.getText().contains("N/A")) {
+				afterStats.add(stat.getText());
+				statGain.add(Double.parseDouble(stat.getAttribute("data-gain")));
+			}
+		}
+		level = Integer.parseInt(driver.findElement(By.cssSelector(".slider>.float-right")).getText());
+		for (int i = 0; i < beforeStats.size(); i++) {
+			total = calculateIncrease(statGain.get(i), Double.parseDouble(beforeStats.get(i)), level);
+			total = Math.round(total * 100.0) / 100.0;
+			statIncreased = (total + 0.01 == Double.parseDouble(afterStats.get(i))
+					|| total - 0.01 == Double.parseDouble(afterStats.get(i))
+					|| total == Double.parseDouble(afterStats.get(i)));
+			if (statIncreased == false) {
+				break;
+			}
+		}
+		Assert.assertEquals(statIncreased, true);
+	}
+
+	@DataProvider
+	public Object[][] heroData() {
+		return new Object[][] { new Object[] { "abathur" }, new Object[] { "jaina" }, new Object[] { "butcher" } };
+	}
+
+	@Test(dataProvider = "heroData")
+	public void regenIncreaseTest(String hero) {
+		ArrayList<WebElement> stats = new ArrayList<WebElement>();
+		ArrayList<String> beforeStats = new ArrayList<String>();
+		ArrayList<String> afterStats = new ArrayList<String>();
+		ArrayList<Double> statGain = new ArrayList<Double>();
+		boolean statIncreased = true;
+		int level;
+		double total;
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".choose-hero a[href*=" + hero)))
+				.click();
+		stats.add(wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".stats.mb10>div:nth-child(2) [data-base]"))));
+		for (WebElement stat : stats) {
+			beforeStats.add(stat.getText());
+		}
+		stats.clear();
+		action.dragAndDropBy(
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ui-slider-handle"))), 20, 0)
+				.perform();
+		stats.add(wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".stats.mb10>div:nth-child(2) [data-gain]"))));
 		for (WebElement stat : stats) {
 			if (!stat.getText().contains("N/A")) {
 				afterStats.add(stat.getText());
